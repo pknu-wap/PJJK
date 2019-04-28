e9d1a72f87fcf02b85fbd1f3101c849c2d8792c6
JeongbinPark <qkrwjdqls99@gmail.com>
2019-04-28 11:06:24 +0900

    add DB file

---
+++ changedDB.js
@@ -0,0 +1,308 @@
+var music_obj = [
+  {
+    id : 0,
+    genre : 3,
+    year : 4,
+    title : 'chase the paper',
+    src : '50 Cent-01-Chase The Paper (Feat. Prodigy, Styles P & Kidd Kidd)-128'},
+  {
+    id : 1,
+    genre : 3,
+    year : 4,
+    title : 'animal ambition',
+    src : '50 Cent-03-Animal Ambition-128'},
+  {
+    id : 2,
+    genre : 4,
+    year : 4,
+    title : 'let me love you',
+    src : 'Ne-Yo-01-Let Me Love You (Until You Learn To Love Yourself)-128'},
+  {
+    id : 3,
+    genre : 7,
+    year : 4,
+    title : 'timber'
+  , src : 'Pitbull-01-Timber (Feat. Ke$ha)-128'},
+  {
+    id : 4,
+    genre : 4,
+    year : 3,
+    title : 'hate that i love you'
+  , src : 'Rihanna-01-Hate That I Love You Feat. Ne-Yo-128'},
+  {
+    id : 5,
+    genre : 3,
+    year : 4,
+    title : '다른 사람'
+  , src : '다알아_팬텀'},
+  {
+    id : 6,
+    genre : 3,
+    year : 4,
+    title : '나나'
+  , src : '박재범-01-나나 (NaNa) (Feat. 로꼬 & AOMG)-128'},
+  {
+    id : 7,
+    genre : 3,
+    year : 4,
+    title : '연결고리'
+  , src : '일리네어 레코즈-03-연결고리 (Feat. MC Meta)-128'},
+  {
+    id : 8,
+    genre : 4,
+    year : 4,
+    title : 'whatever you do'
+  , src : '크러쉬-04-Whatever You Do (Feat. Gray)-128'},
+  {
+    id : 9,
+    genre : 3,
+    year : 4,
+    title : 'freestyle tutorial'
+  , src : '허클베리피-04-Freestyle Tutorial (Feat. Olltii)'}
+  ,{
+    id : 10,
+    genre : 3,
+    year : 4,
+    title : 'love is over'
+  , src : '랍티미스트 (Loptimist) - Love Is Over (Feat. Paloalto & Dead P & 남예지)'},
+  {
+    id : 11,
+    genre : 3,
+    year : 4,
+    title : '그때 우리는'
+  , src : '랍티미스트 (Loptimist) - 그때 우리는'},
+  {
+    id : 12,
+    genre : 3,
+    year : 4,
+    title : '나를 불러본다'
+  , src : '랍티미스트 (Loptimist) - 나를 불러본다 (Feat. D.C)'},
+  {
+    id : 13,
+    genre : 3,
+    year : 4,
+    title : '연어'
+  , src : '랍티미스트 (Loptimist) - 연어 (Feat. Eluphant)'},
+  {
+    id : 14,
+    genre : 3,
+    year : 4,
+    title : '자유로'
+  , src : '랍티미스트 (Loptimist) - 자유로 (Feat. Paloalto & Jinbo)'},
+  {
+    id : 15,
+    genre : 1,
+    year : 4,
+    title : '몸만와'
+  , src : '몸만와 (With 버벌진트)_팬텀'},
+  {
+    id : 16,
+    genre : 1,
+    year : 4,
+    title : '불편화음'
+  , src : '불협화음_팬텀'},
+  {
+    id : 17,
+    genre : 1,
+    year : 4,
+    title : '신세계'
+  , src : '신세계 (Feat. 나비)_팬텀'},
+  {
+    id : 18,
+    genre : 3,
+    year : 4,
+    title : 'we here 2'
+  , src : '일리네어 레코즈-01-We Here 2-128'},
+  {
+    id : 19,
+    genre : 3,
+    year : 4,
+    title : '11:11'
+  , src : '일리네어 레코즈-02-11_11-128'}
+  ,{
+    id : 20,
+    genre : 3,
+    year : 4,
+    title : '가'
+  , src : '일리네어 레코즈-04-가-128'},
+  {
+    id : 21,
+    genre : 3,
+    year : 4,
+    title : '아닌척'
+  , src : '일리네어 레코즈-05-아닌 척 (Don`t Front) (Feat. Jay Park)-128'},
+  {
+    id : 22,
+    genre : 3,
+    year : 4,
+    title : 'a better tomorrow'
+  , src : '일리네어 레코즈-06-A Better Tomorrow (Feat. Sean2slow & MC Meta)-128'},
+  {
+    id : 23,
+    genre : 3,
+    year : 4,
+    title : 'go hard'
+  , src : '일리네어 레코즈-07-Go Hard (Feat. Zion.T)-128'},
+  {
+    id : 24,
+    genre : 3,
+    year : 4,
+    title : 'rollie up'
+  , src : '일리네어 레코즈-08-Rollie Up-128'},
+  {
+    id : 25,
+    genre : 3,
+    year : 4,
+    title : 'we gon\' make it'
+  , src : '일리네어 레코즈-09-We Gon` Make It-128'},
+  {
+    id : 26,
+    genre : 3,
+    year : 4,
+    title : 'ratchet'
+  , src : '일리네어 레코즈-10-Ratchet-128'},
+  {
+    id : 27,
+    genre : 3,
+    year : 4,
+    title : 'illionaire gang 2'
+  , src : '일리네어 레코즈-11-Illionaire Gang 2-128'},
+  {
+    id : 28,
+    genre : 3,
+    year : 4,
+    title : '오늘따라'
+  , src : '팬텀-03-오늘따라 (Feat. 가인)-128'},
+  {
+    id : 29,
+    genre : 3,
+    year : 4,
+    title : '먼저가요'
+  , src : '프리즈몰릭-01-먼저가요 (Feat. Esbee Tale)-128'},
+  {
+    id : 30,
+    genre : 3,
+    year : 4,
+    title : '어리진 않지만 젊어'
+  , src : '프리즈몰릭-04-어리진 않지만 젊어-128'},
+  {
+    id : 31,
+    genre : 3,
+    year : 4,
+    title : 'more than a dream'
+  , src : '허클베리피-01-More Than A Dream'},
+  {
+    id : 32,
+    genre : 3,
+    year : 4,
+    title : 'my life so bright'
+  , src : '허클베리피-02-My Life So Bright'},
+  {
+    id : 33,
+    genre : 3,
+    year : 4,
+    title : 'man in black'
+  , src : '허클베리피-03-Man In Black'},
+  {
+    id : 34,
+    genre : 3,
+    year : 4,
+    title : '남자의 자격'
+  , src : '허클베리피-05-남자의 자격 (Feat. B-Free, Rhyme-A-)'},
+  {
+    id : 35,
+    genre : 3,
+    year : 4,
+    title : 'the battlefield'
+  , src : '허클베리피-06-The Battlefield (Feat. MC META, JJK)'},
+  {
+    id : 36,
+    genre : 3,
+    year : 4,
+    title : 'history is made at night'
+  , src : '허클베리피-07-History Is Made At Night (Feat. Simon Dominic)'},
+  {
+    id : 37,
+    genre : 3,
+    year : 4,
+    title : 'man in black (remix)'
+  , src : '허클베리피-08-Man In Black (Remix) (Feat. Jerry.K, Minos, Vasco, Paloalto)'},
+  {
+    id : 38,
+    genre : 3,
+    year : 4,
+    title : 'que sera sera'
+  , src : '화나-04-Que Sera Sera-128'},
+  {
+    id : 39,
+    genre : 3,
+    year : 4,
+    title : '신발끈 블루스'
+  , src : '화나-07-신발끈 블루스-128'}
+  ,{
+    id : 40,
+    genre : 3,
+    year : 4,
+    title : '껌'
+  , src : '화나-10-껌-128'},
+  {
+    id : 41,
+    genre : 3,
+    year : 4,
+    title : '미쳤어'
+  , src : 'Beenzino-03-미쳤어 (Feat. Don Mills)-128'},
+  {
+    id : 42,
+    genre : 3,
+    year : 4,
+    title : 'up all night'
+  , src : 'Beenzino-04-Up All Night (Feat. Mayson The Soul)-128'},
+  {
+    id : 43,
+    genre : 1,
+    year : 4,
+    title : 'future wife'
+  , src : 'Future Wife_팬텀'},
+  {
+    id : 44,
+    genre : 1,
+    year : 4,
+    title : 'phantom power'
+  , src : 'PHANTOM POWER_팬텀'},
+  {
+    id : 45,
+    genre : 1,
+    year : 4,
+    title : 'under age\'s song'
+  , src : 'Under Age\'s Song (Feat. 휘인 of 마마무) (키겐 Solo)_팬텀'},
+  {
+    id : 46,
+    genre : 1,
+    year : 4,
+    title : '내가 졌다'
+  , src : '내가 졌다_팬텀'},
+  {
+    id : 47,
+    genre : 1,
+    year : 4,
+    title : '너와'
+  , src : '너와 (산체스 Solo)_팬텀'},
+  {
+    id : 48,
+    genre : 1,
+    year : 4,
+    title : '다알아'
+  , src : '다알아_팬텀'},
+  {
+    id : 49,
+    genre : 3,
+    year : 4,
+    title : 'boogie night'
+  , src : '랍티미스트 (Loptimist) - Boogie Night (Feat. Makesense & D.C)'},
+  {
+    id : 50,
+    genre : 3,
+    year : 4,
+    title : 'how do i look'
+  , src : 'Beenzino-02-How Do I Look _-128'},
+]
